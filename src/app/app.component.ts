c639275ff7643f2dfcb463e4aff0c0aeef794d96
rootrya <rootrya@gmail.com>
2021-04-17 23:21:22 -0500

    pie sample
    sample graph for Ilirie's numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'legend-lore';
+
+  chartData = [
+    {
+      data: [58, 10, 18, 0, 0, 0, 0, 0, 0],
+      label: 'Calais'
+    }
+    // {
+    //   data: [0, 0, 0, 22, 26, 7, 16, 0, 0],
+    //   label: 'Ilirie'
+    // },
+    // {
+    //   data: [0, 0, 0, 24, 0, 0, 0, 10, 42],
+    //   label: 'Kemvari'
+    // },
+    // {
+    //   data: [33, 4, 0, 0, 0, 0, 0, 0, 4],
+    //   label: 'Ojjun'
+    // },
+    // {
+    //   data: [0, 0, 22, 5, 0, 0, 0, 25, 0],
+    //   label: 'Viktor'
+    // }
+  ];
+
+  chartLabels = [
+    'Radiant',
+    'Slashing',
+    'Necrotic'
+    // 'Fire',
+    // 'Psychic',
+    // 'Cold',
+    // 'Poison',
+    // 'Force',
+    // 'Bludgeoning'
+  ];
+
+  chartOptions = {
+    responsive: true
+  };
+
+  onChartHover = ($event: any) => {
+    window.console.log('onChartHover', $event);
+  };
+
+  onChartClick = ($event: any) => {
+    window.console.log('onChartClick', $event);
+  };
+
+  public chartColors: Array < any > = [{
+    backgroundColor: ['rgba(218,165,32,.8)',
+    'rgba(169,169,169,.8)',
+    'rgba(46,139,87,.8)'
+  ]
+ }]; 
 }
