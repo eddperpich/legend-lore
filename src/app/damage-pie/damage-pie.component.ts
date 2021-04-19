a54bfa29f7e66321f88933090b1ef3b7c05eb8d4
rootrya <rootrya@gmail.com>
2021-04-18 20:02:33 -0500

    messing with color arrays
    damage: color

--- src/app/damage-pie/damage-pie.component.ts
+++ src/app/damage-pie/damage-pie.component.ts
@@ -21,6 +21,7 @@ export class DamagePieComponent implements OnInit {
 
   chartLabels: string[] = [];
   chartData: SingleDataSet[] = [];
+  chartColors: Array<any> = [{backgroundColors: []}];
 
   title = 'legend-lore';
   form: FormGroup;
@@ -64,9 +65,15 @@ export class DamagePieComponent implements OnInit {
     }
   ];
 
-  public chartColors: Array<any> = [{
-    backgroundColor: ['rgba(218,165,32,.8)', 'rgba(169,169,169,.8)', 'rgba(46,139,87,.8)']
-  }];
+  // public chartColors: Array<any> = [{
+  //   backgroundColor: ['rgba(218,165,32,.8)', 'rgba(169,169,169,.8)', 'rgba(46,139,87,.8)']
+  // }];
+  
+  public colorCodes = {
+      'NECROTIC': 'rgba(218,165,32,.8)',
+      'COLD': 'rgba(169,169,169,.8)',
+      'RADIANT': 'rgba(46,139,87,.8)'
+  };
 
   chartOptions = {
     responsive: true
@@ -106,10 +113,14 @@ export class DamagePieComponent implements OnInit {
   }
 
   render(): void {
-    const damages: { [p: string]: number } = this.rawData
-      .filter(item => item.player === this.form.get('player').value)
-      .reduce((base, value) => ({...base, [value.dType]: (base[value.dType] || 0) + value.damage}), {});
-    [this.chartLabels, this.chartData] = Object.entries(damages).reduce((v1, v2) => [[...v1[0], v2[0]], [...v1[1], v2[1]]], [[], []]);
+    const damages: { [p: string]: number } = this.rawData // create damages, a [p: string]:num array from rawData
+      .filter(item => item.player === this.form.get('player').value) // filter, checking each object.player of rawdata = the selected form player
+      .reduce((base, value) => ({...base, [value.dType]: (base[value.dType] || 0) + value.damage}), {}); // reduce into key:value pair of damage of dType or 0 + current value damage
+    [this.chartLabels, this.chartData, this.chartColors] = Object.entries(damages).reduce((v1, v2) => [
+      [...v1[0], v2[0]],
+      [...v1[1], v2[1]],
+      [...v1[2], this.colorCodes[v2[0]]]],
+      [[], [], []]
+      ); //reduce into a [[]] array
   }
-
 }
